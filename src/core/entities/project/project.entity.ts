68628f51fc2732215e526cceeb9aa14ecb208989
Emant <emantweb@gmail.com>
2022-12-07 05:53:17 +0300

    Core domain modified, user shouldn't have to know other dependencies feature that they depend on it

--- src/core/entities/project/project.entity.ts
+++ src/core/entities/project/project.entity.ts
@@ -1,6 +1,7 @@
 import { User } from '../user/user.entity';
 
 export class Project {
+  user: User;
   title: string;
   subTitle: string;
   description: string;
--- src/core/entities/user/user.entity.ts
+++ src/core/entities/user/user.entity.ts
@@ -1,9 +1,4 @@
-import { Project } from '../project/project.entity';
-import { Resume } from '../resume/resume.entity';
-
 export class User {
   email: string;
   password: string;
-  resume: Resume;
-  projects: Project[];
 }
